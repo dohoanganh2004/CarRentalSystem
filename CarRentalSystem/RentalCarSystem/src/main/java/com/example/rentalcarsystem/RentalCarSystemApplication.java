22ba1f16cbfb7d6dd31a0b0bbf13bc6a8d7b46ce
dohoanganh2004 <dohoanganh92004@gmail.com>
2025-05-29 08:16:56 +0700

    Add files via upload

---
+++ CarRentalSystem/RentalCarSystem/src/main/java/com/example/rentalcarsystem/RentalCarSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalcarsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarSystemApplication.class, args);
+    }
+
+}
