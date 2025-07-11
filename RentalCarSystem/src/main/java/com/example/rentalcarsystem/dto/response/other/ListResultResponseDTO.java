5fe1c7f8a6812a5722910ce2ecd6343e533708da
dohoanganh2004 <dohoanganh92004@gmail.com>
2025-07-11 11:02:00 +0700

    remove secret config from repository

---
+++ RentalCarSystem/src/main/java/com/example/rentalcarsystem/dto/response/other/ListResultResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.rentalcarsystem.dto.response.other;
+
+public class ListResultResponseDTO {
+}
---
+++ RentalCarSystem/src/main/java/com/example/rentalcarsystem/sercutiry/JwtProperties.java
@@ -0,0 +1,4 @@
+package com.example.rentalcarsystem.sercutiry;
+
+public class JwtProperties {
+}
